e11874ec6ee9c53e3e746327bce45cb928e6be59
shaxowskiy1 <spavelin@inbox.ru>
2026-03-01 16:19:12 +0500

    init commit

---
+++ src/main/java/ru/shaxowskiy/javaurfuhomework/JavaUrfuHomeworkApplication.java
@@ -0,0 +1,13 @@
+package ru.shaxowskiy.javaurfuhomework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaUrfuHomeworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JavaUrfuHomeworkApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/shaxowskiy/javaurfuhomework/JavaUrfuHomeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.shaxowskiy.javaurfuhomework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaUrfuHomeworkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
