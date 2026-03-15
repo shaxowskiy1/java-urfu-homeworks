a50dc3795f570f034d37fa665b56b965718ba46b
shaxowskiy1 <spavelin@inbox.ru>
2026-03-15 11:26:05 +0500

    add main to last task

--- src/main/java/ru/shaxowskiy/javaurfuhomework/Main.java
+++ src/main/java/ru/shaxowskiy/javaurfuhomework/Main.java
@@ -2,6 +2,10 @@ package ru.shaxowskiy.javaurfuhomework;
 
 public class Main {
     public static void main(String[] args) {
+        Animal dog = new Dog("Dog", 1);
+        dog.makeSound();
 
+        Animal bird = new Bird("Bird", 1);
+        System.out.println(bird.getName());
     }
 }
